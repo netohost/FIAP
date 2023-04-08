ba64c0ec797f03d9bf168d31a690bcf0868df620
João Neto <103568777+netohost@users.noreply.github.com>
2023-04-08 02:06:05 -0300

    Add files via upload

---
+++ so_teste.py
@@ -0,0 +1,8 @@
+import platform
+
+print("Nome Máquina.........: ", platform.node)
+print("Arquitetura..........: ", platform.architecture())
+print("Sistema Operacional..: ", platform.system())
+print("Versão S.O...........: ", platform.release())
+print("Processador..........: ", platform.processor())
+print("Versão Python........: ", platform.python_version())
